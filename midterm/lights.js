b01b338a37f57d51e1c06ca1ecd07eb2f312d2ba
Boyan Sun <sunboyan1097335645@gmail.com>
2023-10-30 20:24:24 -0400

    midterm

---
+++ midterm/lights.js
@@ -0,0 +1,7 @@
+class light{
+    constructor(x,y,s){
+        this.x = x;
+        this.y = y;
+        this.s = s;
+    }
+}
--- midterm/midterm.js
+++ midterm/midterm.js
@@ -1,20 +1,96 @@
-let x,y;
-
+let wl11, wl12, wl21, w22;
+let move;
+let wlx, wly;
+let fwd, bwd;
+let mvx, mvy, mvmag;
 
 function setup(){
   createCanvas(800,800);
-  x = width/2;
-  y = height/2;
+  noStroke();
+  wlx = 375;
+  wly = 375*tan((44.5/180)*PI);
+  mvx = 50;
+  mvy = height-2*wly;
+  mvmag = pow(mvx,2)+pow(mvy,2);
+  wl11 = createVector(wlx,wly);
+  wl12 = createVector(wlx,height-wly);
+  wl21 = createVector(wlx+50,wly);
+  wl22 = createVector(wlx+50,height-wly);
+  move = createVector(mvx/(sqrt(mvmag)),mvy/(sqrt(mvmag)));
+  fwd = false;
+  bwd = false;
+
+}
+
+function wall(){
+  fill(105);
+  beginShape();
+  vertex(0,0);
+  vertex(wl11.x, wl11.y);
+  vertex(wl12.x, wl12.y);
+  vertex(0,800);
+  endShape();
+  beginShape();
+  vertex(800,0);
+  vertex(wl21.x, wl21.y);
+  vertex(wl22.x, wl22.y);
+  vertex(800,800);
+  endShape();
+}
+
+function ground(){
+  fill(128);
+  beginShape();
+  vertex(0,0);
+  vertex(wl11.x, wl11.y);
+  vertex(wl21.x, wl21.y);
+  vertex(800,0);
+  endShape();
+  beginShape();
+  vertex(0,800);
+  vertex(wl12.x, wl12.y);
+  vertex(wl22.x, wl22.y);
+  vertex(800,800);
+  endShape();
+}
+
+function keyPressed(){
+  if(key == 'w'){
+    fwd = true;
+  }
+  else if(key == 's'){
+    bwd = true;
+  }
+}
+
+function keyReleased(){
+  if(key == 'w'){
+    fwd = false;
+  }
+  else if(key == 's'){
+    bwd = false;
+  }
 }
 
 function draw(){
-  background(0);
-  x = map(mouseX,0,width,350,450); //map remaps and returns a number, in this case remap mouseX from (0,width) to (350,450)
-  fill(127);
-  stroke(240,220,27);
-  strokeWeight(4);
-  ellipse(x,y,40,40);
-  print(x);
+  background(80);
+  wall();
+  ground();
+  if(fwd){
+    wl11.add(-move.x,-move.y);
+    wl12.add(-move.x,move.y);
+    wl21.add(move.x,-move.y);
+    wl22.add(move.x,move.y);
+    print(abs(wl11.y-wl12.y)/abs(wl11.x-wl21.x));
+  }
+  else if(bwd && abs(wl11.x-wl21.x) >= 15){
+    wl11.add(move.x,move.y);
+    wl12.add(move.x,-move.y);
+    wl21.add(-move.x,move.y);
+    wl22.add(-move.x,-move.y);
+    print(abs(wl11.y-wl12.y)/abs(wl11.x-wl21.x));
+    print(abs(wl11.x-wl21.x));
+  }
   
 
 }
